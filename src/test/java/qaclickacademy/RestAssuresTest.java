ad271805daf5733218279b34908cf6d6973ed2ca
pinkashia <pinkashia92@gmail.com>
2020-09-02 12:18:57 +0530

    my first commit

---
+++ src/test/java/qaclickacademy/RestAssuresTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RestAssuresTest {
+
+	@Test
+	public void API_Automation()
+	{
+		System.out.println("API_Automation");
+	}
+	
+	@Test
+	public void Get_Response()
+	{
+		System.out.println("Get_Response");
+	}
+}
---
+++ src/test/java/qaclickacademy/SeliniumTest.java
@@ -0,0 +1,19 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeliniumTest {
+	
+	@Test
+	public void Browser_Automation()
+	{
+		System.out.println("Browser_Automation");
+	}
+	
+	@Test
+	public void User_SignIn()
+	{
+		System.out.println("User_Signin");
+	}
+
+}
